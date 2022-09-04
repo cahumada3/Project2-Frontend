909863884639b0e792ba933382052b8c5fb6668c
Evan Owre <eowre@email.sc.edu>
2022-09-04 13:17:28 -0400

    debuging auth

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -25,8 +25,8 @@ export const msalConfig: Configuration = {
     auth: {
         clientId:"12b471b2-cdab-402d-9eb9-30ccdef11f36",
         authority: b2cPolicies.authorities.signUpSignIn.authority,
-        redirectUri: "https://TeleComApp.telecomb2c.co",
-        postLogoutRedirectUri: "https://TeleComApp.telecomb2c.co",
+        redirectUri: "https://calm-beach-035463210.1.azurestaticapps.net/",
+        postLogoutRedirectUri: "https://calm-beach-035463210.1.azurestaticapps.net/",
         navigateToLoginRequestUrl: true,
         validateAuthority: true,
     },
